021f79e018be3d494504438bd1bb91f925f6a54c
sangeethkc <keyceesangi@gmail.com>
2025-01-18 14:15:53 +0530

    change in app

--- app/run.py
+++ app/run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Flask Webserver!"
+    return "Welcome to The Webserver!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
